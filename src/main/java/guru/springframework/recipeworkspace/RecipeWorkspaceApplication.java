9ab73973ceb194b4b53ab9adeb70b2283339747a
Ranjani Harish <ranjani.harish@ey.com>
2020-10-26 06:37:24 +0530

    Establish JPA relationships between Recipe, Ingredients, Notes and UnitOfMeasure

---
+++ src/main/java/guru/springframework/recipeworkspace/RecipeWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.recipeworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeWorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeWorkspaceApplication.class, args);
+    }
+
+}
